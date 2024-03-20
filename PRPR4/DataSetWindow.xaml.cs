3b5ee645c2a443e7a2df236e8e01cd7e6251f2f8
NikolaiGric <nikolaigricak@gmail.com>
2024-03-20 15:26:52 +0300

    Добавьте файлы проекта.

---
+++ PRPR4/DataSetWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using PRPR4.Cinema_practicDataSetTableAdapters;
+
+namespace PRPR4
+{
+    public partial class DataSetWindow : Window
+    {
+        MoviesTableAdapter movies = new MoviesTableAdapter();
+        FilmsTableAdapter films = new FilmsTableAdapter();
+        public DataSetWindow()
+        {
+            InitializeComponent();
+            AllDataGrid.ItemsSource = movies.GetData();
+            AllCombobox.ItemsSource = films.GetData();
+            AllCombobox.DisplayMemberPath = "Name_Film";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (SearchTxt.Text != null) 
+            {
+                AllDataGrid.ItemsSource = movies.SearchByAge_Rating(SearchTxt.Text); 
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AllDataGrid.ItemsSource = movies.GetData();
+        }
+
+        private void AllCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AllCombobox.SelectedItem != null)
+            {
+                var id = (int)(AllCombobox.SelectedItem as DataRowView).Row[0];
+                AllDataGrid.ItemsSource = movies.FilterbyFilms(id);
+            }
+        }
+    }
+}
---
+++ PRPR4/EntityFrameworkWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PRPR4
+{
+    /// <summary>
+    /// Логика взаимодействия для EntityFrameworkWindow.xaml
+    /// </summary>
+    public partial class EntityFrameworkWindow : Window
+    {
+        private Cinema_practicEntities context = new Cinema_practicEntities();
+        public EntityFrameworkWindow()
+        {
+            InitializeComponent();
+
+            AllDataGrid.ItemsSource = context.Movies.ToList();
+            AllCombobox.ItemsSource = context.Films.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AllDataGrid.ItemsSource = context.Movies.ToList().Where(x => x.Age_Rating.Contains(SearchTxt.Text));
+            AllCombobox.ItemsSource = context.Films.ToList();
+        }   
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AllDataGrid.ItemsSource = context.Movies.ToList();
+        }
+        private void AllCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AllCombobox.SelectedItem != null)
+            {
+                var selected = AllCombobox.SelectedItem as Films;
+                AllDataGrid.ItemsSource = context.Movies.ToList().Where(x => x.Film_ID == selected.ID_Films);
+            }
+        }
+    }
+}
